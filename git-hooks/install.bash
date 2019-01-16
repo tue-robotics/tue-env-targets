97dd46fb68bff8ebdfe266be5d7522550350ed06
Arpit Aggarwal <ar13pit@gmail.com>
2019-01-16 23:27:53 +0100

    added new variable to keep script readable

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -1,5 +1,8 @@
+#! /usr/bin/env bash
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+
 # install the global hooks
-git config --global core.hooksPath $(dirname "${BASH_SOURCE[0]}")/git_hooks/
+git config --global core.hooksPath $script_dir/git_hooks/
 
 if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
     tue-install-warning "Please perform a
--- sergio-chrony-client-pc/install.bash
+++ sergio-chrony-client-pc/install.bash
@@ -1,6 +1,8 @@
-## Chrony
+#! /usr/bin/env bash
+script_dir=$(dirname "${BASH_SOURCE[0]}")
 set -e
 
+## Chrony
 # If config file does not exist, chrony is probably not installed 
 if [ ! -f /etc/chrony/chrony.conf ]
 then
@@ -9,13 +11,13 @@ then
 fi
 
 # Check config file
-if ! cmp /etc/chrony/chrony.conf $(dirname "${BASH_SOURCE[0]}")/chrony.conf --quiet
+if ! cmp /etc/chrony/chrony.conf $script_dir/chrony.conf --quiet
 then
     echo "Chrony config is probably not correct, will copy"
-    
+
     # Copy and backup old config
-    sudo install --backup=numbered --compare --verbose $(dirname "${BASH_SOURCE[0]}")/chrony.conf /etc/chrony/chrony.conf
-    
+    sudo install --backup=numbered --compare --verbose $script_dir/chrony.conf /etc/chrony/chrony.conf
+
     # Restart chrony
     sudo service chrony restart
 fi
