c7e321c676220325c908a4037bca88197aff3a61
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-16 23:04:38 +0100

    fix gitsu target in more robust way

--- gitsu/install.bash
+++ gitsu/install.bash
@@ -2,9 +2,22 @@
 hash gem 2> /dev/null || tue-install-system-now ruby
 hash git-su 2> /dev/null || sudo gem install gitsu
 
+gitsu_file=~/.gitsu
 # install the authors file
-if [ ! -f ~/.gitsu ]
+if [ -L $gitsu_file ]
 then
-    echo "linking ~/.gitsu"
-    ln -s $(dirname "${BASH_SOURCE[0]}")/gitsu.txt ~/.gitsu
+   if [ -e $gitsu_file ]
+   then
+      tue-install-debug "gistu file: good link"
+   else
+      tue-install-debug "gistu file: link is broken and will be replaced"
+      rm $gitsu_file
+      ln -s $(dirname "${BASH_SOURCE[0]}")/gitsu.txt $gitsu_file
+   fi
+elif [ -e ${my_link} ]
+then
+   tue-install-debug "gistu file: it is not a link"
+else
+    tue-install-debug "gistu file: link doesn't exist, but will be created"
+   ln -s $(dirname "${BASH_SOURCE[0]}")/gitsu.txt $gitsu_file
 fi
