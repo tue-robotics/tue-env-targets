b62104f0e8bc55d8d88e7e8fe00397207d583580
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-30 23:25:24 +0100

    WIP cuda installer

--- cuda/install.bash
+++ cuda/install.bash
@@ -1,9 +1,34 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb -o /tmp/cuda.deb
+ubuntu_version=$(lsb_release -rs)
+ubuntu_version="${ubuntu_version//./}"
+cuda_version="10.0.130-1"
+if [ $(uname -p) == "x86_64" ]
+then
+    architecture="amd64"
+else
+    architecture="ppc64el"
+fi
 
-sudo dpkg -i /tmp/cuda.deb
+tue-install-debug "Installing cuda with following configuration:\nUbuntu version: ${ubuntu_version}\nCuda version: ${cuda_version}\nArchitecture: ${architecture}"
 
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+cuda_file="cuda-repo-ubuntu${ubuntu_version}_${cuda_version}_${architecture}.deb"
+cuda_url="""https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/${cuda_file}"
 
-sudo apt-get update
+if [ -f /tmp/$cuda_file ]
+then
+    tue-install-debug "Removing old cuda debian: /tmp/$cuda_file"
+    rm -f /tmp/$cuda_file
+fi
+
+tue-install-debug "wget $cuda_url"
+wget $cuda_url -P /tmp
+
+tue-install-debug "sudo dpkg -i /tmp/$cuda_file"
+sudo dpkg -i /tmp/$cuda_file
+
+tue-install-debug "sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub"
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub
+
+tue-install-debug "sudo apt-get update -qq"
+sudo apt-get update -qq
 
 tue-install-system cuda
