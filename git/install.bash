e435a213cfe9c501064522d96bf14df1ccd54ac6
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-02-04 11:17:48 +0100

    update git target; always reset global ignore file

--- git/install.bash
+++ git/install.bash
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 # check for git-extras
-if [ ! -f /usr/local/bin/git-extras ]; then
+if [ ! -f /usr/local/bin/git-extras ]
+then
     # https://github.com/tj/git-extras
     cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install
-    
-    # enable color
-    git config --global color.ui true
 fi
 
-if [[ ! $(git config --global core.excludesfile) ]]; then
-    # enable global ignore file
-    git config --global core.excludesfile $(dirname "${BASH_SOURCE[0]}")/gitignore_global
+# Always overwrite global ignore file
+global_git_ignore=$(dirname "${BASH_SOURCE[0]}")/gitignore_global
+if [[ $(git config --global core.excludesfile)!=$global_git_ignore ]]
+then
+    git config --global core.excludesfile $global_git_ignore
+fi
+
+# Set lg alias
+if ! git config --global --get alias.lg > /dev/null
+then
+    git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+fi
+
+# Set push default
+if ! git config --global --get push.default > /dev/null
+then
+    git config --global push.default "current"
+fi
+
+# enable color
+if ! git config --global --get color.ui > /dev/null
+then
+    git config --global color.ui true
 fi
