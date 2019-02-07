4c01248e72706b1c3a6975df2a58ec249492ce11
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-02-07 15:10:26 +0100

    replace tabs by spaces in cuda target

--- cuda/install.bash
+++ cuda/install.bash
@@ -13,44 +13,44 @@ fi
 
 if [ -e /usr/local/cuda ]
 then
-		if [ $cuda_version_local == cuda_version ]
-		then
-			return
-		else
-			sudo apt-get purge -y cuda*
-			sudo apt-get purge libcudnn*
-			sudo apt-get purge libcudnn*-dev
-			install_cuda=true
-		fi
+    if [ $cuda_version_local == cuda_version ]
+    then
+        return
+    else
+        sudo apt-get purge -y cuda*
+        sudo apt-get purge libcudnn*
+        sudo apt-get purge libcudnn*-dev
+        install_cuda=true
+fi
 else
-	install_cuda=true
+    install_cuda=true
 fi
 
 if [[ $install_cuda==true ]]
 then
-	tue-install-debug "Installing cuda with following configuration:\nUbuntu version: ${ubuntu_version}\nCuda version: ${cuda_version}\nArchitecture: ${architecture}"
+    tue-install-debug "Installing cuda with following configuration:\nUbuntu version: ${ubuntu_version}\nCuda version: ${cuda_version}\nArchitecture: ${architecture}"
 
-	return
-	cuda_file="cuda-repo-ubuntu${ubuntu_version}_${cuda_version}_${architecture}.deb"
-	cuda_url="""https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/${cuda_file}"
+    return
+    cuda_file="cuda-repo-ubuntu${ubuntu_version}_${cuda_version}_${architecture}.deb"
+    cuda_url="""https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/${cuda_file}"
 
-	if [ -f /tmp/$cuda_file ]
-	then
-		tue-install-debug "Removing old cuda debian: /tmp/$cuda_file"
-		rm -f /tmp/$cuda_file
-	fi
+    if [ -f /tmp/$cuda_file ]
+    then
+        tue-install-debug "Removing old cuda debian: /tmp/$cuda_file"
+        rm -f /tmp/$cuda_file
+    fi
 
-	tue-install-debug "wget $cuda_url"
-	wget $cuda_url -P /tmp
+    tue-install-debug "wget $cuda_url"
+    wget $cuda_url -P /tmp
 
-	tue-install-debug "sudo dpkg -i /tmp/$cuda_file"
-	sudo dpkg -i /tmp/$cuda_file
+    tue-install-debug "sudo dpkg -i /tmp/$cuda_file"
+    sudo dpkg -i /tmp/$cuda_file
 
-	tue-install-debug "sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub"
-	sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub
+    tue-install-debug "sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub"
+    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub
 
-	tue-install-debug "sudo apt-get update -qq"
-	sudo apt-get update -qq
+    tue-install-debug "sudo apt-get update -qq"
+    sudo apt-get update -qq
 
-	tue-install-system cuda
+    tue-install-system cuda
 fi
