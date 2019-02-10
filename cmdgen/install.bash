6ca397fa10447b5458f5993f19e5a25b19dcd317
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-02-10 01:57:53 +0100

    cmdgen ssh proof; small refactor

--- cmdgen/install.bash
+++ cmdgen/install.bash
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+github_url="https://github.com/tue-robotics/GPSRCmdGen.git"
+
 # by default, set the previous commit to -1, which will trigger a 'make'
 prev="-1"
 
 if [ -d ~/src/GPSRCmdGen ]; then # if the directory already exists
     cd ~/src/GPSRCmdGen
-    REMOTE=$(git config --get remote.origin.url) # get the remote
-    
-    # if the GSPRCmdGen is pointing to the wrong Remote, correct it
-    if [ "$REMOTE" != "https://github.com/tue-robotics/GPSRCmdGen.git" ]; then
+    current_url=$(git config --get remote.origin.url) # get the remote
+
+    # if the GSPRCmdGen is pointing to the wrong remote, correct it
+    if [ "$(_github_https "$current_url")" != "$github_url" ]
+    then
         tue-install-debug "The GPSRCmdGen is still pointing to old remote, will be changed to tue-fork"
-        git remote set-url origin https://github.com/tue-robotics/GPSRCmdGen.git
+        git remote set-url origin $github_url
     fi
 
     # Git is set-up correctly, so record the previous commit
@@ -18,7 +21,7 @@ if [ -d ~/src/GPSRCmdGen ]; then # if the directory already exists
 fi
 
 # tue-install-git will decide if clone or pull is needed
-tue-install-git https://github.com/tue-robotics/GPSRCmdGen.git ~/src/GPSRCmdGen
+tue-install-git $github_url ~/src/GPSRCmdGen
 
 # install mono if not yet installed
 hash mono 2> /dev/null || tue-install-system-now mono-complete
