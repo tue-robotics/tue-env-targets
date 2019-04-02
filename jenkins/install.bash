892fa6a7a65a6a33082a0beed2ebbfbc5ea128e8
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-02 13:36:13 +0200

    fix jenkins target

--- jenkins/install.bash
+++ jenkins/install.bash
@@ -1,11 +1,10 @@
 #! /usr/bin/env bash
 
-if [ -f ! /etc/apt/sources.list.d/jenkins.list ]
+if [ ! -f "/etc/apt/sources.list.d/jenkins.list" ]
 then
-
-	wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-	sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-	sudo apt-get update
+    wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+    sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt-get update
 fi
 
 tue-install-system jenkins
