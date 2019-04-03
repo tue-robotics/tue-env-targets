5bbfadd86007e79725452709a065d4c9ba60d47f
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 16:59:50 +0200

    fix chrony client targets linting

--- hero-chrony-client-pc/install.bash
+++ hero-chrony-client-pc/install.bash
@@ -1,20 +1,15 @@
 #! /usr/bin/env bash
 set -e
 
-# If config file does not exist, chrony is probably not installed
-if [ ! -f /etc/chrony/chrony.conf ]
-then
-    echo "I guess chrony is not installed"
-    sudo apt-get install --assume-yes chrony
-fi
+tue-install-system-now chrony
 
 # Check config file
-if ! cmp /etc/chrony/chrony.conf $(dirname "${BASH_SOURCE[0]}")/chrony.conf --quiet
+if ! cmp /etc/chrony/chrony.conf "$(dirname "${BASH_SOURCE[0]}")"/chrony.conf --quiet
 then
     echo "Chrony config is probably not correct, will copy"
 
     # Copy and backup old config
-    sudo install --backup=numbered --compare --verbose $(dirname "${BASH_SOURCE[0]}")/chrony.conf /etc/chrony/chrony.conf
+    sudo install --backup=numbered --compare --verbose "$(dirname "${BASH_SOURCE[0]}")"/chrony.conf /etc/chrony/chrony.conf
 
     # Restart chrony
     sudo service chrony restart
