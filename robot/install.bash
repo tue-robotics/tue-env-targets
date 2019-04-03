53c09db57267035cff499ae7832c5191c568fab4
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 22:01:19 +0200

    fix robot lint

--- robot/install.bash
+++ robot/install.bash
@@ -5,7 +5,7 @@ tue-install-cp ros-bash-and-run.sh ~/.ros-bash-and-run.sh
 chmod +x ~/.ros-bash-and-run.sh
 
 ### Allow user to access serial interfaces ###
-if ! groups ${USER} | grep -q dialout
+if ! groups "${USER}" | grep -q dialout
 then
-    sudo gpasswd --add ${USER} dialout
+    sudo gpasswd --add "${USER}" dialout
 fi
--- robot/ros-bash-and-run.sh
+++ robot/ros-bash-and-run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /usr/bin/env sh
 
 cmd=""
 for arg in "$@"
