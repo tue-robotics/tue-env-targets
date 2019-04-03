0f66388069ccc339a58afa67693b1a93ab9b6f51
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 17:26:37 +0200

    fix git-hooks lint

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -5,7 +5,6 @@ git config --global core.hooksPath "$(dirname "${BASH_SOURCE[0]}")"/git_hooks/
 
 if dpkg --compare-versions "$(git --version | awk '{print $3}')" lt 2.9
 then
-    tue-install-warning "Please perform a
-    sudo apt install git
-afterwards to upgrade the git version to allow git-hooks"
+    tue-install-info "Running sudo apt-get install --assume-yes git to upgrade git>2.9"
+    sudo apt-get install --assume-yes git
 fi
