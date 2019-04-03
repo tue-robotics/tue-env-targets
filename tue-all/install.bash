fe7a98fdc812fcd0a3135c33ffd827f50f638348
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 23:03:59 +0200

    fix tue-all lint

--- tue-all/install.bash
+++ tue-all/install.bash
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
-targets=$(ls $TUE_ENV_TARGETS_DIR)
+targets="$TUE_ENV_TARGETS_DIR/*"
 for target in $targets
 do
+    target="$(basename "$target")"
     if [[ $target != "tue-all" ]]
     then
-        tue-install-target $target
+        tue-install-target "$target"
     fi
 done
