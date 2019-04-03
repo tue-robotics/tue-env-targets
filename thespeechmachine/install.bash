c900dae6d4d08b9972c64ef2ce8d73d5718282fa
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 22:57:42 +0200

    fix thespeechmachine lint

--- thespeechmachine/install.bash
+++ thespeechmachine/install.bash
@@ -3,7 +3,7 @@
 # Install vbox if not installed
 if ! dpkg -s virtualbox-4.3 &> /dev/null
 then
-    URL='http://download.virtualbox.org/virtualbox/4.3.20/virtualbox-4.3_4.3.20-96996~Ubuntu~precise_amd64.deb'; FILE=`mktemp`; wget "$URL" -qO $FILE && sudo dpkg -i $FILE; rm $FILE
+    URL='http://download.virtualbox.org/virtualbox/4.3.20/virtualbox-4.3_4.3.20-96996~Ubuntu~precise_amd64.deb'; FILE=$(mktemp); wget "$URL" -qO "$FILE" && sudo dpkg -i "$FILE"; rm "$FILE"
 fi
 
 # Download and unzip if vbox not there
