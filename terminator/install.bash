67a663176ca5e75d1c1e17beab75adc46f1af7bb
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 22:56:37 +0200

    fix terminator lint

--- terminator/install.bash
+++ terminator/install.bash
@@ -2,17 +2,17 @@
 
 if [ ! -d ~/.config/terminator ]
 then
-    tue-install-debug "creating ~/.config/terminator, because not existing yet"
-    mkdir -p ~/.config/terminator
+    tue-install-debug "creating $HOME/.config/terminator, because not existing yet"
+    mkdir -p "$HOME"/.config/terminator
 else
-    tue-install-debug "~/.config/terminator already exists"
+    tue-install-debug "$HOME/.config/terminator already exists"
 fi
 
 if [ -f ~/.config/terminator/config ]
 then
-    tue-install-debug "tue-install-cp config ~/.config/terminator/config2"
-    tue-install-cp config ~/.config/terminator/config2
+    tue-install-debug "tue-install-cp config $HOME/.config/terminator/config2"
+    tue-install-cp config "$HOME"/.config/terminator/config2
 else
-    tue-install-debug "tue-install-cp config ~/.config/terminator/config"
-    tue-install-cp config ~/.config/terminator/config
+    tue-install-debug "tue-install-cp config $HOME/.config/terminator/config"
+    tue-install-cp config "$HOME"/.config/terminator/config
 fi
