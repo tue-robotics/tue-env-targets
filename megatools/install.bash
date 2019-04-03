e31b7be0557d99c9cdda60461e02249e53a5bbc8
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-03 17:56:21 +0200

    fix megatools lint

--- megatools/install.bash
+++ megatools/install.bash
@@ -8,20 +8,20 @@ DATA_DIR="$HOME/MEGA"
 if [ ! -d "$DATA_DIR" ]; then
     # Create data dir
     echo "Creating MEGA dir in home"
-    mkdir $DATA_DIR
+    mkdir -p "$DATA_DIR"
 fi
 
 # Setup mega rc file
 if [ ! -f "$HOME/.megarc" ]; then
     # Create .megarc file in $HOME
     echo "Copying megarc file to home"
-    cp $DIR/.megarc $HOME/.megarc
+    cp "$DIR"/.megarc "$HOME"/.megarc
 
     echo "Please type the MEGA password of the amigoathome@gmail.com account"
-    read -s -p password: passwd
+    read -r -s -p password: passwd
 
     echo "Adding password to .megarc file"
-    echo "Password = $passwd" >> $HOME/.megarc
+    echo "Password = $passwd" >> "$HOME"/.megarc
 
     echo "Setting permissions to .megarc file"
     chmod 640 ~/.megarc
@@ -33,8 +33,8 @@ if [ ! -f "/etc/systemd/system/mega.timer" ]; then
     # Create mega service
     echo "Copying mega service files to /etc/systemd/system"
 
-    sudo cp $DIR/mega.service /etc/systemd/system/
-    sudo cp $DIR/mega.timer /etc/systemd/system/
+    sudo cp "$DIR"/mega.service /etc/systemd/system/
+    sudo cp "$DIR"/mega.timer /etc/systemd/system/
 
     # Reload and enable
     sudo systemctl daemon-reload
