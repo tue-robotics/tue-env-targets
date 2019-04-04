00abe100464394c27aab7abbd3bdd2fd9a3e3cb2
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-04 12:01:50 +0200

    [megatools] fix if style

--- megatools/install.bash
+++ megatools/install.bash
@@ -5,14 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create MEGA DIR is not exists
 DATA_DIR="$HOME/MEGA"
-if [ ! -d "$DATA_DIR" ]; then
+if [ ! -d "$DATA_DIR" ]
+then
     # Create data dir
     echo "Creating MEGA dir in home"
     mkdir -p "$DATA_DIR"
 fi
 
 # Setup mega rc file
-if [ ! -f "$HOME/.megarc" ]; then
+if [ ! -f "$HOME/.megarc" ]
+then
     # Create .megarc file in $HOME
     echo "Copying megarc file to home"
     cp "$DIR"/.megarc "$HOME"/.megarc
@@ -29,7 +31,8 @@ fi
 
 
 # Setup the sync service
-if [ ! -f "/etc/systemd/system/mega.timer" ]; then
+if [ ! -f "/etc/systemd/system/mega.timer" ]
+then
     # Create mega service
     echo "Copying mega service files to /etc/systemd/system"
 
