7713d33acd49d8624eacb5104c52822b542d19a8
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-04 16:25:28 +0200

    [ros-dashboard] use $DISTRIB_RELEASE directly

--- ros-dashboard/install.bash
+++ ros-dashboard/install.bash
@@ -7,15 +7,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck disable=SC1091
 source /etc/lsb-release
 
-VER="$DISTRIB_RELEASE"
-if [ "$VER" == "14.04" ]
+if [ "$DISTRIB_RELEASE" == "14.04" ]
 then
-    if ! [ -L /usr/lib/libudev.so.0 ];
+    if ! [ -L /usr/lib/libudev.so.0 ]
     then
         echo "I'm going to symlink libudev.so.1.3.5 to libudev.so.0"
         sudo ln -s /lib/x86_64-linux-gnu/libudev.so.1.3.5 /usr/lib/libudev.so.0
     fi
-elif [ "$VER" == "16.04" ]
+elif [ "$DISTRIB_RELEASE" == "16.04" ]
 then
     if ! [ -L /lib/x86_64-linux-gnu/libudev.so.0 ]
     then
