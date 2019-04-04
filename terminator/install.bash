2782364e7f788d5c065a5add86d1aaf882397206
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-04 16:41:38 +0200

    [terminator] use tilde again, but add shellcheck disable for the use of ~ in print statements

--- terminator/install.bash
+++ terminator/install.bash
@@ -2,17 +2,21 @@
 
 if [ ! -d ~/.config/terminator ]
 then
-    tue-install-debug "creating $HOME/.config/terminator, because not existing yet"
-    mkdir -p "$HOME"/.config/terminator
+    # shellcheck disable=SC2088
+    tue-install-debug "creating ~/.config/terminator, because not existing yet"
+    mkdir -p ~/.config/terminator
 else
-    tue-install-debug "$HOME/.config/terminator already exists"
+    # shellcheck disable=SC2088
+    tue-install-debug "~/.config/terminator already exists"
 fi
 
 if [ -f ~/.config/terminator/config ]
 then
-    tue-install-debug "tue-install-cp config $HOME/.config/terminator/config2"
-    tue-install-cp config "$HOME"/.config/terminator/config2
+    # shellcheck disable=SC2088
+    tue-install-debug "tue-install-cp config ~/.config/terminator/config2"
+    tue-install-cp config ~/.config/terminator/config2
 else
-    tue-install-debug "tue-install-cp config $HOME/.config/terminator/config"
-    tue-install-cp config "$HOME"/.config/terminator/config
+    # shellcheck disable=SC2088
+    tue-install-debug "tue-install-cp config ~/.config/terminator/config"
+    tue-install-cp config ~/.config/terminator/config
 fi
