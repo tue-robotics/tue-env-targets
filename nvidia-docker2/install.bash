5f29bf6b8d8f2dd28c24671eb7b888ff1c9bf26d
Arpit Aggarwal <ar13pit@gmail.com>
2019-05-13 13:47:41 +0200

    added shellcheck disable

--- nvidia-docker2/install.bash
+++ nvidia-docker2/install.bash
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+
+# shellcheck disable=SC1091
 distribution="$(. /etc/os-release;echo "$ID$VERSION_ID")"
+
 curl -s -L https://nvidia.github.io/nvidia-docker/"$distribution"/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
