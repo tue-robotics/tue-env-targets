0900e70d59033215303aa88b9d8a10bec4293149
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-06-08 11:18:18 +0200

    [ros] remove old apt key

--- ros/install.bash
+++ ros/install.bash
@@ -28,6 +28,7 @@ fi
 if [ -z "$(apt-key list | grep 4096R/AB17C654)" ]
 then
     sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
 fi
 
 if [ ! -f /tmp/rosdep_update ]
