56f3c2c2c75e2c4058f4aa279c6e3655d1eceeda
Arpit Aggarwal <ar13pit@gmail.com>
2019-06-08 21:55:39 +0200

    [ros] remove temp code

--- ros/install.bash
+++ ros/install.bash
@@ -24,13 +24,6 @@ then
     sudo rosdep init || true # make sure it always succeeds, even if rosdep init was already called
 fi
 
-# TEMP fix for to only update the key
-if ! apt-key list | grep -q 4096R/AB17C654
-then
-    sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-fi
-
 if [ ! -f /tmp/rosdep_update ]
 then
     tue-install-debug "Updating rosdep"
