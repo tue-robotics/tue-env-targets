8562e51af4f4121c8c25098643e8a29008342542
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2019-10-15 19:23:24 +0200

    [python-tensorflow-pip] tensorflow>=2.0

--- python-tensorflow-pip/install.bash
+++ python-tensorflow-pip/install.bash
@@ -2,7 +2,7 @@
 
 if [ -n "$TUE_CUDA" ]
 then
-    tue-install-pip 'tensorflow-gpu<3.0'
+    tue-install-pip 'tensorflow-gpu>=2.0,<3.0'
 else
-    tue-install-pip 'tensorflow<3.0'
+    tue-install-pip 'tensorflow>=2.0,<3.0'
 fi
