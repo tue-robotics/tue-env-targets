18762f660a817aead270e87e43a8151631c7ded3
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2019-10-24 20:10:52 +0200

    [matthijs] add shortcuts

--- matthijs/install.bash
+++ matthijs/install.bash
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+# Geany config
 if [ ! -f ~/.config/geany/geany.conf ]
 then
     tue-install-debug "geany config not existing"
@@ -9,6 +10,7 @@ else
     tue-install-debug "geany config does exists, so not copying matthijs config"
 fi
 
+# Git Settings
 git_config_items="pull.ff=true color.ui=always merge.tool=meld"
 for item in $git_config_items
 do
@@ -17,3 +19,8 @@ do
         tue-install-debug "git config --global --replace-all $option $value"
         git config --global --replace-all "$option" "$value"
 done
+
+# Shortcuts
+tue-install-cp shortcuts/pycharm-community.desktop ~/Desktop
+tue-install-cp shortcuts/qtcreator.desktop ~/Desktop
+tue-install-cp shortcuts/terminator.desktop ~/.local/share/applications
