658e6a5369d866b1af885a8934b711ab03c47bd2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2019-12-23 11:32:00 +0100

    [hsr-setup] sync with upstream

--- hsr-setup/install.bash
+++ hsr-setup/install.bash
@@ -6,8 +6,10 @@ then
     sudo sh -c 'echo "deb https://hsr-user:jD3k4G2e@packages.hsr.io/ros/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/tmc.list'
     sudo sh -c 'echo "deb https://hsr-user:jD3k4G2e@packages.hsr.io/tmc/ubuntu `lsb_release -cs` multiverse main" >> /etc/apt/sources.list.d/tmc.list'
     sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+    sudo sh -c 'echo "deb https://apache.bintray.com/couchdb-deb `lsb_release -cs` main" > /etc/apt/sources.list.d/couchdb.list'
     wget https://hsr-user:jD3k4G2e@packages.hsr.io/tmc.key -O - | sudo apt-key add -
     wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+    wget https://couchdb.apache.org/repo/bintray-pubkey.asc -O - | sudo apt-key add -
     sudo apt-get update -qq
     tue-install-debug "Added HSR sources to apt-get succesfully"
 else
