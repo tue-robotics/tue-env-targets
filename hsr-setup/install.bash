96060d2bf5354618e681c287d7da39f5cb8e63b8
Arpit Aggarwal <ar13pit@gmail.com>
2020-01-04 19:29:26 +0100

    refactor(hsr-setup): Remove gazebo source setup and make it a dependency

--- hsr-setup/install.bash
+++ hsr-setup/install.bash
@@ -12,12 +12,10 @@ then
     fi
 
     tue-install-debug "Adding HSR sources to apt-get"
-    sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
     sudo sh -c 'echo "deb https://packages.hsr.io/ros/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/tmc.list'
     sudo sh -c 'echo "deb https://packages.hsr.io/tmc/ubuntu `lsb_release -cs` multiverse main" >> /etc/apt/sources.list.d/tmc.list'
 
     wget https://hsr-user:jD3k4G2e@packages.hsr.io/tmc.key -O - | sudo apt-key add -
-    wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
     sudo apt-get update -qq
     tue-install-debug "Added HSR sources to apt-get succesfully"
