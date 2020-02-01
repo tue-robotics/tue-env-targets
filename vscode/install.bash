b94acc7306fa4e2cbf24f0900e9a2ac38cdb1a5b
Tim Clephas <tim.clephas@nobleo.nl>
2020-02-01 12:48:06 +0100

    Configure VScode for nobleo-env environment

--- vscode/install.bash
+++ vscode/install.bash
@@ -1,8 +1,30 @@
 #! /usr/bin/env bash
 
-if [ ! -f /etc/apt/sources.list.d/vscode.list ]
+# Install vscode via snap
+if [[ ! $(snap list) == *code* ]]
 then
-	wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O /tmp/vscode.deb
-	sudo dpkg -i /tmp/vscode.deb
-	sudo apt-get install --assume-yes -f # Install dependencies
+    tue-install-snap-now code
+fi
+tue-install-system-now default-jre
+
+# Install ros extension
+code --install-extension ms-iot.vscode-ros
+
+# Configure workspace
+workspacefile="${TUE_SYSTEM_DIR}/.vscode/settings.json"
+mkdir -p "${TUE_SYSTEM_DIR}/.vscode"
+if [[ ! -f "${workspacefile}" ]]
+then
+    tue-install-cp workspace_settings.json "${workspacefile}"
+else
+    tue-install-system-now jq
+    jq -s add "${workspacefile}" "${TUE_INSTALL_CURRENT_TARGET_DIR}/workspace_settings.json" > "${workspacefile}.new"
+    [ $? -eq 0 ] && mv "${workspacefile}.new" "${workspacefile}"  # Doesn't work at once above
+fi
+
+# Install and configure catkin tools extension
+code --install-extension betwo.b2-catkin-tools
+if ! catkin config --workspace ${TUE_SYSTEM_DIR} | grep -q "DCMAKE_EXPORT_COMPILE_COMMANDS"
+then
+    catkin config --workspace ${TUE_SYSTEM_DIR} --append-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 fi
