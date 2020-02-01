afc626c3cf4c9c0954601ef325ebe66df04a66ca
Tim Clephas <tim.clephas@nobleo.nl>
2020-02-01 12:28:57 +0100

    Only upgrade git if required

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 # install the global hooks
-git config --global core.hooksPath "$(dirname "${BASH_SOURCE[0]}")"/git_hooks/
-
-if dpkg --compare-versions "$(git --version | awk '{print $3}')" lt 2.9
-then
-    tue-install-info "Running sudo apt-get install --assume-yes git to upgrade git>2.9"
-    sudo apt-get install --assume-yes git
+if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
+    echo "Need to upgrade git to use the git-hooks feature"
+    nobleo-install-ppa ppa:git-core/ppa
+    echo "sudo apt install git"
+    sudo apt install --assume-yes git
 fi
+
+git config --global core.hooksPath $NOBLEO_DIR/installer/targets/git-hooks/git_hooks/
