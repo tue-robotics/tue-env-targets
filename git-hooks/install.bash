5df5358fdd71aeb78171d35fa5d9a0850223965d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-02 00:00:35 +0100

    [git-hooks] use apt-get

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -5,7 +5,7 @@ if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
     echo "Need to upgrade git to use the git-hooks feature"
     tue-install-ppa ppa:git-core/ppa
     echo "sudo apt install git"
-    sudo apt install --assume-yes git
+    sudo apt-get install --assume-yes git
 fi
 
 git config --global core.hooksPath $NOBLEO_DIR/installer/targets/git-hooks/git_hooks/
