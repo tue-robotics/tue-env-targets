338c8f28820b5c1d2729b0093f4fd0e48e27378f
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-11 19:50:20 +0100

    [hero-demo-laptop] add terminator shortcut

--- hero-demo-laptop/install.bash
+++ hero-demo-laptop/install.bash
@@ -3,3 +3,6 @@
 # RVIZ shortcut
 tue-install-cp shortcuts/hero-rviz.desktop ~/Desktop
 tue-install-cp shortcuts/icons/rviz.png /usr/share/pixmaps/tue/rviz.png
+
+# Terminator
+tue-install-cp shortcuts/terminator.desktop /usr/share/applications
