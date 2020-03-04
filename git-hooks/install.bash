e9cdacc5cff7df2787cb0cb237e84ed90a652575
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-05 00:27:38 +0100

    [git-hooks] fix shellcheck

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
 # install the global hooks
-if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
+if dpkg --compare-versions "$(git --version | awk '{print $3}')" lt 2.9
+then
     tue-install-info "Upgrade git to > 2.9 for compatibility of git-hooks feature"
-    tue-install-ppa-now	 ppa:git-core/ppa
+    tue-install-ppa-now ppa:git-core/ppa
     # Running here apt-get manually to force an update
     echo "[git-hooks] running command: sudo apt-get install git"
     sudo apt-get install --assume-yes git
