12a01e8074abb029937f2ede71cfa671164a8ea4
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-04 15:56:24 +0100

    [git-hooks] update install

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -2,9 +2,10 @@
 
 # install the global hooks
 if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
-    echo "Need to upgrade git to use the git-hooks feature"
-    tue-install-ppa ppa:git-core/ppa
-    echo "sudo apt-get install git"
+    tue-install-info "Upgrade git to > 2.9 for compatibility of git-hooks feature"
+    tue-install-ppa-now	 ppa:git-core/ppa
+    # Running here apt-get manually to force an update
+    echo "[git-hooks] running command: sudo apt-get install git"
     sudo apt-get install --assume-yes git
 fi
 
