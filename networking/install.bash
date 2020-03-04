633cac8387b75359f0dc69a47ce0444b2fb2a81c
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-05 00:29:37 +0100

    [networking] fix shellcheck

--- networking/install.bash
+++ networking/install.bash
@@ -7,7 +7,7 @@ then
 fi
 
 . /etc/os-release
-if [ $UBUNTU_CODENAME = "xenial" ]
+if [ "$UBUNTU_CODENAME" = "xenial" ]
 then
     # Install config file (mdns4_minimal is normally missing in xenial and should be present)
 
