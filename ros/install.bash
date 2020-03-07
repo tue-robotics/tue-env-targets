fff7655380f984a6f38bc741cc775fbe0553ac86
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-07 12:49:17 +0100

    Revert "[catkin][ros] move ws construction to catkin"
    This reverts commit 631f7584ad8f17419a07a8f6718c9d37128d8ddb.

--- ros/install.bash
+++ ros/install.bash
@@ -22,3 +22,15 @@ then
     rosdep update
     touch "$rosdep_update_file"
 fi
+
+mkdir -p "$TUE_SYSTEM_DIR" "$TUE_DEV_DIR"
+
+if [ ! -f "$TUE_SYSTEM_DIR"/devel/setup.bash ]
+then
+    tue-make || tue-install-error "Error in building the system workspace"
+fi
+
+if [ ! -f "$TUE_DEV_DIR"/devel/setup.bash ]
+then
+    tue-make-dev || tue-install-error "Error in building the dev workspace"
+fi
