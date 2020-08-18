75993a649ec690e6e4d33cf6706a824b93c7c83e
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-08-18 11:34:35 +0200

    [hero-demo-laptop] install rviz shortcut to local apps

--- hero-demo-laptop/install.bash
+++ hero-demo-laptop/install.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # RVIZ shortcut
-tue-install-cp shortcuts/hero-rviz.desktop ~/Desktop
+tue-install-cp shortcuts/hero-rviz.desktop ~/.local/share/applications
 tue-install-cp shortcuts/icons/rviz.png /usr/share/pixmaps/tue/rviz.png
 
 # Terminator
