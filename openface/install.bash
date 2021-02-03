4e63be7c8bca4883cc6a58875df7bca51be07a29
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-02-03 16:40:25 +0100

    [openface] use new dlib target; cleanup

--- openface/install.bash
+++ openface/install.bash
@@ -1,31 +1,33 @@
 #! /usr/bin/env bash
 
-if [ ! -d ~/openface ]
+if [ ! -d "${HOME}"/openface ]
 then
     tue-install-git https://github.com/cmusatyalab/openface.git  ~/openface
 
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     # shellcheck disable=SC1090
-    source "$DIR"/dlib.bash
+    # source "$DIR"/dlib.bash
     # shellcheck disable=SC1090
     source "$DIR"/torch.bash
 
     # shellcheck disable=SC2164
-    cd ~/openface
-    sudo python2 setup.py install
+    cd "${HOME}"/openface
+    python2 setup.py install --user
 
     models/get-models.sh
+fi
 
-    if [[ $(python -c "import dlib") -eq 1 ]]; then
-        echo "DLIB is not properly installed"
-    fi
+if [[ $(python2 -c "import dlib") -eq 1 ]]
+then
+    tue-install-error "DLIB is not properly installed"
+fi
 
-    if [[ $(python -c "import cv2") -eq 1 ]]; then
-        echo "opencv2 is not properly installed"
-    fi
+if [[ $(python2 -c "import cv2") -eq 1 ]]
+then
+    tue-install-error "opencv2 is not properly installed"
+fi
 
-    if [[ $(python -c "import openface") -eq 1 ]]; then
-        echo "openface is not properly installed"
-    fi
-
+if [[ $(python2 -c "import openface") -eq 1 ]]
+then
+    tue-install-error "openface is not properly installed"
 fi
