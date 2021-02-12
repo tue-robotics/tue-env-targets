3e3da67120317c8b720d49ba5719b2f6e774c314
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-02-12 17:41:42 +0100

    Apply tue-intall-apt-get-update

--- dropbox/install.bash
+++ dropbox/install.bash
@@ -4,7 +4,7 @@ if [ ! -f /etc/apt/sources.list.d/dropbox.list ]
 then
     sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
     sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ trusty main" >> /etc/apt/sources.list.d/dropbox.list'
-    sudo apt-get update
+    tue-install-apt-get-update
 fi
 
 tue-install-system dropbox
