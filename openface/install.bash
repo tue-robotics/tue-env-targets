4d7163f7928e576bfd206186ff54561572dcbd8d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-02-23 00:10:13 +0100

    [openface] Fully python3 compaitble

--- openface/install.bash
+++ openface/install.bash
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+pv=3
+if [ $TUE_ROS_DISTRO == melodic ] || [ $TUE_ROS_DISTRO == xenial ]
+then
+    pv=2
+fi
+
 if [ ! -d "${HOME}"/openface ]
 then
     tue-install-git https://github.com/cmusatyalab/openface.git  ~/openface
@@ -12,22 +18,22 @@ then
 
     # shellcheck disable=SC2164
     cd "${HOME}"/openface
-    python2 setup.py install --user
+    python${pv} setup.py install --user
 
     models/get-models.sh
 fi
 
-if [[ $(python2 -c "import dlib") -eq 1 ]]
+if [[ $(python${pv} -c "import dlib") -eq 1 ]]
 then
     tue-install-error "DLIB is not properly installed"
 fi
 
-if [[ $(python2 -c "import cv2") -eq 1 ]]
+if [[ $(python${pv} -c "import cv2") -eq 1 ]]
 then
     tue-install-error "opencv2 is not properly installed"
 fi
 
-if [[ $(python2 -c "import openface") -eq 1 ]]
+if [[ $(python${pv} -c "import openface") -eq 1 ]]
 then
     tue-install-error "openface is not properly installed"
 fi
