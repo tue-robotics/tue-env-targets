8bdd6688ab8b23b77e74de818c83db89fe1f584d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-04-26 13:55:31 +0200

    [git-hooks] fix targets path

--- git-hooks/install.bash
+++ git-hooks/install.bash
@@ -6,8 +6,7 @@ then
     tue-install-info "Upgrade git to > 2.9 for compatibility of git-hooks feature"
     tue-install-ppa-now ppa:git-core/ppa
     # Running here apt-get manually to force an update
-    echo "[git-hooks] running command: sudo apt-get install git"
-    sudo apt-get install --assume-yes git
+    tue-install-pipe sudo apt-get install --assume-yes git
 fi
 
-git config --global core.hooksPath "$TUE_DIR"/installer/targets/git-hooks/git_hooks/
+git config --global core.hooksPath "$TUE_ENV_TARGETS_DIR"/git-hooks/git_hooks/
