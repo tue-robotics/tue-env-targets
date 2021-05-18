fcac486a310db597f6dc5a4520c2de13563c6e84
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-05-18 23:32:43 +0200

    [gazebo_plugins] remove unused git dir

--- ros-gazebo_plugins/install.bash
+++ ros-gazebo_plugins/install.bash
@@ -3,6 +3,7 @@
 if [ "$TUE_ROS_DISTRO" != "melodic" ] && [ -L "$TUE_SYSTEM_DIR"/src/gazebo_plugins ]
 then
     tue-install-info "Removing gazebo_plugins from workspace, using apt version again"
+    rm -rf "$(git -C "$(realpath "$TUE_SYSTEM_DIR"/src/gazebo_plugins)" rev-parse --show-toplevel)"
     rm "$TUE_SYSTEM_DIR"/src/gazebo_plugins
     catkin clean --workspace "$TUE_SYSTEM_DIR" --orphans
 fi
