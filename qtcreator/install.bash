8df1d19ebca0f60ac784695e7786b95afa84a4d4
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-05-23 10:13:05 +0200

    [qtcreator] use tue logging functions

--- qtcreator/install.bash
+++ qtcreator/install.bash
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 
 DIRECTORY='/opt/Qt'
-if [ ! -d "$DIRECTORY" ]; then
-    wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run -O /tmp/qt-open-source-installer
-    chmod +x /tmp/qt-open-source-installer
+if [ ! -d "$DIRECTORY" ]
+then
+    tue-install-pipe wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run -O /tmp/qt-open-source-installer
+    tue-install-pipe chmod +x /tmp/qt-open-source-installer
 
-    echo ""
-    echo "Choose $DIRECTORY as install directory!"
-    echo ""
+    tue-install-tee ""
+    tue-install-echo "Choose $DIRECTORY as install directory!"
+    tue-install-tee ""
 
-    sudo /tmp/qt-open-source-installer
+    tue-install-pipe sudo /tmp/qt-open-source-installer
 fi
 
