aae5fbc71dc4eeec33471bfb4af022f8642dcb7b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-05-30 09:10:56 +0200

    [ros-setup] check and update expired GPG key

--- ros-setup/install.bash
+++ ros-setup/install.bash
@@ -5,10 +5,17 @@ then
     tue-install-echo "Adding ROS sources to apt-get"
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    tue-install-pipe sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
     tue-install-apt-get-update
     tue-install-debug "Added ROS sources to apt-get successfully"
 else
     tue-install-debug "ROS sources already added to apt-get"
+fi
+
+# Check for expired key and update
+if sudo apt-key adv --list-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 2>/dev/null | grep -q expired
+then
+    tue-install-echo "Updating expired GPG key"
+    tue-install-pipe sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 fi
