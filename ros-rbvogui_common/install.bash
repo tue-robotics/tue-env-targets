60112a5fd7a893432a5553481972b1dfe15f8ad1
Juan D. Hernandez <HernandezVegaJ@cardiff.ac.uk>
2021-09-08 03:50:44 -0700

    Using IROHMS_ROS_DISTRO to determine the ROS distribution

--- ros-rbvogui_common/install.bash
+++ ros-rbvogui_common/install.bash
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 # Check if robotnik libraries have not been installed yet
-if [ ! -d "/opt/ros/${ROS_DISTRO}/lib/robotnik_twist2ackermann" ]
+if [ ! -d "/opt/ros/${IROHMS_ROS_DISTRO}/lib/robotnik_twist2ackermann" ]
 then
 	echo "Installing robotnik libraries"
-	sudo apt install "ros-${ROS_DISTRO}-nav-msgs"
+	sudo apt install "ros-${IROHMS_ROS_DISTRO}-nav-msgs" "ros-${IROHMS_ROS_DISTRO}-controller-interface"
 	sudo dpkg -i "$(dirname "${IROHMS_ENV_TARGETS_DIR}")"/../../melodic/repos/github.com/RobotnikAutomation/rbvogui_common/libraries/*
 fi
