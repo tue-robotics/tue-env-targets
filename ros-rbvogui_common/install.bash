7b02af6668f6eafb9ee8646936288a4548abb909
Juan D. Hernandez <HernandezVegaJ@cardiff.ac.uk>
2021-09-08 03:26:48 -0700

    Dependency needed for rbvogui_common libraries

--- ros-rbvogui_common/install.bash
+++ ros-rbvogui_common/install.bash
@@ -4,5 +4,6 @@
 if [ ! -d "/opt/ros/${ROS_DISTRO}/lib/robotnik_twist2ackermann" ]
 then
 	echo "Installing robotnik libraries"
+	sudo apt install "ros-${ROS_DISTRO}-nav-msgs"
 	sudo dpkg -i "$(dirname "${IROHMS_ENV_TARGETS_DIR}")"/../../melodic/repos/github.com/RobotnikAutomation/rbvogui_common/libraries/*
 fi
