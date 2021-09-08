04ef47e3f15646cc53f7167fca977f188bff8d6d
Juan D. Hernandez <HernandezVegaJ@cardiff.ac.uk>
2021-09-08 04:53:07 -0700

    Additional packages that must be installed before rbvogui_common

--- ros-rbvogui_common/install.bash
+++ ros-rbvogui_common/install.bash
@@ -4,6 +4,6 @@
 if [ ! -d "/opt/ros/${IROHMS_ROS_DISTRO}/lib/robotnik_twist2ackermann" ]
 then
 	echo "Installing robotnik libraries"
-	sudo apt install "ros-${IROHMS_ROS_DISTRO}-nav-msgs" "ros-${IROHMS_ROS_DISTRO}-controller-interface"
+	sudo apt install "ros-${IROHMS_ROS_DISTRO}-nav-msgs" "ros-${IROHMS_ROS_DISTRO}-controller-interface" "ros-${IROHMS_ROS_DISTRO}-nav-tf" "ros-${IROHMS_ROS_DISTRO}-urdf" "ros-${IROHMS_ROS_DISTRO}-ackermann-msgs" "ros-${IROHMS_ROS_DISTRO}-sensors-msgs"
 	sudo dpkg -i "$(dirname "${IROHMS_ENV_TARGETS_DIR}")"/../../melodic/repos/github.com/RobotnikAutomation/rbvogui_common/libraries/*
 fi
