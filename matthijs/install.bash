a43932801302f9abba454a6bde00935ad69c4536
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-10-22 11:56:01 +0200

    [qtcreator][matthijs] move tue QtCreator desktop file qtcreator target

--- matthijs/install.bash
+++ matthijs/install.bash
@@ -45,13 +45,6 @@ fi
 # Terminator
 tue-install-cp shortcuts/terminator.desktop ~/.local/share/applications/terminator.desktop
 
-# Qt Creator
-if [ -f ~/.local/share/applications/org.qt-project.qtcreator.desktop ] && ! grep -qi matthijs ~/.local/share/applications/org.qt-project.qtcreator.desktop
-then
-    mv -f ~/.local/share/applications/org.qt-project.qtcreator.desktop ~/.local/share/applications/org.qt-project.qtcreator.desktop.bk
-fi
-tue-install-cp shortcuts/org.qt-project.qtcreator.desktop ~/.local/share/applications/org.qt-project.qtcreator.desktop
-
 # WhatsApp
 tue-install-cp shortcuts/whatsapp-webapp.desktop ~/.local/share/applications/whatsapp-webapp.desktop
 tue-install-cp shortcuts/icons/whatsapp.svg /usr/share/icons/hicolor/scalable/apps/whatsapp.svg
