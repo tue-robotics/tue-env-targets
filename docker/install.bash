c4145c9a03addf37fc07ffbf7880e835e443db62
Arpit Aggarwal <ar13pit@gmail.com>
2022-02-05 15:36:31 +0100

    feat(docker): Add debug statements

--- docker/install.bash
+++ docker/install.bash
@@ -9,7 +9,10 @@ else
     # Add stable docker repository source
     if ! apt-key adv --fingerprint 0EBFCD88 &> /dev/null
     then
+        tue-install-debug "Downloading gpg key of docker repo with fingerprint 0EBFCD88..."
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    else
+        tue-install-debug "GPG key of docker repo with fingerprint 0EBFCD88 already exists, so not installing it."
     fi
 
     ppa="deb [arch=${arch}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -20,11 +23,14 @@ else
 
     if [[ $(groups) != *"docker"* ]]
     then
+        tue-install-debug "Adding user ${USER} to docker group and restarting services..."
         sudo usermod -aG docker "${USER}"
 
         # Restart the Docker daemon
         sudo service docker restart
 
         tue-install-info "Added user '${USER}' to docker group. A new login may be required to use docker without sudo."
+    else
+        tue-install-debug "Current user ${USER} already present in docker group."
     fi
 fi
