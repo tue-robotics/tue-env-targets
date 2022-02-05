797f2cc76b77e2cccad05cd8fee1138ea295a66d
Loy van Beek <loy.vanbeek@gmail.com>
2022-02-05 15:52:31 +0100

    Add installer for image_recognition_pytorch

---
+++ ros-image_recognition_pytorch/install.bash
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+url="https://github.com/Nebula4869/PyTorch-gender-age-estimation/raw/038331d26fc1fbf24d00365d0eb9d0e5e828dda6/models-2020-11-20-14-37/best-epoch47-0.9314.onnx"
+dest=~/pytorch/models
+
+if [ ! -f $dest/"$(basename "$url")" ]
+then
+    tue-install-debug "wget $url -P $dest"
+    wget $url -P $dest
+fi
