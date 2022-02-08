62aa759bc6559f0df9204b9bdf33d6042d661d28
Arpit Aggarwal <ar13pit@gmail.com>
2022-02-08 19:01:59 +0100

    feat(image_recognition_pytorch): Update model path
    Co-authored-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- ros-image_recognition_pytorch/install.bash
+++ ros-image_recognition_pytorch/install.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 url="https://github.com/Nebula4869/PyTorch-gender-age-estimation/raw/038331d26fc1fbf24d00365d0eb9d0e5e828dda6/models-2020-11-20-14-37/best-epoch47-0.9314.onnx"
-dest=~/pytorch/models
+dest=~/data/pytorch_models
 
 if [ ! -f $dest/"$(basename "$url")" ]
 then
