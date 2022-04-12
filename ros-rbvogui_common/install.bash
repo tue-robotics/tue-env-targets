6766fd748c905df12c1c3125fee7ad186ae45c4a
Samuelson Atiba <25374425+samuelsont@users.noreply.github.com>
2022-04-12 15:43:58 +0100

    Update rbvogui_common

--- ros-rbvogui_common/install.bash
+++ ros-rbvogui_common/install.bash
@@ -5,5 +5,8 @@ if [ ! -d "/opt/ros/${IROHMS_ROS_DISTRO}/lib/robotnik_twist2ackermann" ]
 then
 	echo "Installing robotnik libraries"
 	sudo apt install "ros-${IROHMS_ROS_DISTRO}-nav-msgs" "ros-${IROHMS_ROS_DISTRO}-controller-interface" "ros-${IROHMS_ROS_DISTRO}-tf" "ros-${IROHMS_ROS_DISTRO}-urdf" "ros-${IROHMS_ROS_DISTRO}-ackermann-msgs" "ros-${IROHMS_ROS_DISTRO}-sensor-msgs"
-	sudo dpkg -i "$(dirname "${IROHMS_ENV_TARGETS_DIR}")"/../../melodic/repos/github.com/RobotnikAutomation/rbvogui_common/libraries/*
+	sudo dpkg -i "${IROHMS_ENV_DIR}"/repos/github.com/RobotnikAutomation/rbvogui_common/libraries/*
+
+else
+  echo "Requirements already met. Robotnik Libraries installed"
 fi
