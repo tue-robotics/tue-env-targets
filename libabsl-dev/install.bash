d782ffed88f10201914c4554f47269b54ed37d12
sasilva1998 <sasilva1998@gmail.com>
2022-10-03 11:55:36 +0100

    fix: check correct abseil dir

--- libabsl-dev/install.bash
+++ libabsl-dev/install.bash
@@ -5,8 +5,8 @@ else
   set -o errexit
   set -o verbose
 
-  git clone https://github.com/abseil/abseil-cpp.git
-  cd abseil-cpp
+  git clone https://github.com/abseil/abseil-cpp.git /home/${USER}/ros/${ROS_DISTRO}/system/src/abseil-cpp
+  cd /home/${USER}/ros/${ROS_DISTRO}/system/src/abseil-cpp
   git checkout 215105818dfde3174fe799600bb0f3cae233d0bf # 20211102.0
   mkdir build
   cd build
