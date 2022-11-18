3f7969a13d743f665da1317c82dbdceb69d14aff
Steven A. Silva <sasilva1998@gmail.com>
2022-11-18 14:41:58 +0000

    Tiago Simulation targets (#22)
    * chore: custom end effector
    
    * chore: omni base nav
    
    * chore: pal gripper
    
    * chore: tiago deps
    
    * chore: pal gazebo
    
    * chore: pal hardware
    
    * chore: pal msgs
    
    * chore: pal nav cfgs
    
    * chore: pal deps
    
    * chore: pal robotiq gripper
    
    * chore: pal statistics
    
    * chore: pal wsg gripper
    
    * chore: play motion
    
    * chore: pmb2 nav
    
    * chore: pmb2 robot
    
    * chore: pmb2 nav sim
    
    * chore: simple robot
    
    * chore: tiago deps
    
    * chore: tiago supp
    
    * chore: tiago sim
    
    * fix: subdirs missing
    
    * chore: deps tiago sim
    
    * chore: deps tiago sim
    
    * chore: tiago demo pkgs
    
    * chore: tiago missing deps
    
    * fix: missing gz repos
    
    * fix: folder names mising
    
    * chore: tiago gazebo dep
    
    * fix: python ver

---
+++ ros-tiago_gazebo/install.bash
@@ -0,0 +1,5 @@
+git clone https://github.com/CardiffUniversityComputationalRobotics/ros_control /home/${USER}/ros/noetic/system/src/ros_control
+
+git clone https://github.com/pal-robotics-forks/ros_controllers /home/${USER}/ros/noetic/system/src/ros_controllers
+
+git clone --branch noetic-devel https://github.com/pal-robotics-forks/gazebo_ros_pkgs /home/${USER}/ros/noetic/system/src/gazebo_ros_pkgs
