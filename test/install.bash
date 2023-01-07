2881547c04fbc521dcad6107a19a48ea53b0d838
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-01-07 08:54:32 +0100

    [test][temp] test target

---
+++ test/install.bash
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+echo -e "Just some shit"
+echo -e "Just some shit2"
+tue-install-info "bla blaa blaat"
+echo -e "Just some shit3"
+
+tue-install-gem-now gitsu
+
+tue-install-pipe sudo apt-get update
+
+tue-install-cp blaat.bash /opt/ros/noetic
+
+echo -e "Just some shit4"
