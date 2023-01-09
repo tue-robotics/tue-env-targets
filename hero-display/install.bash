146b079deafa43749a1428b1acb78df3ee29952f
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-01-09 16:14:11 +0100

    [hero-display] not allowed to capture stdin

--- hero-display/install.bash
+++ hero-display/install.bash
@@ -3,7 +3,8 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+releases=$("$DIR"/get-releases)
 # shellcheck disable=SC1091
-"$DIR"/get-releases | source "$DIR"/process-releases
+source "$DIR"/process-releases "${releases}"
 
 set +e
