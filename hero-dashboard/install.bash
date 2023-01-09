ad076145eeee954733b5ccbe8be93a2c5045b434
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-01-09 16:13:28 +0100

    [hero-dashboard] drop removing of older release folder

--- hero-dashboard/install.bash
+++ hero-dashboard/install.bash
@@ -1,12 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-if [ -f "$TUE_BIN"/hero_dashboard.d/hero-dashboard ]
-then
-    tue-install-debug "Removing old release folder"
-    rm -rf "$TUE_BIN"/hero_dashboard.d
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # shellcheck disable=SC1091
