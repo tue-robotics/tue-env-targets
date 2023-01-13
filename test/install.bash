6b40c079d464b9055866888424f3a45d1876a80d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-01-13 12:46:41 +0100

    [test] update

--- test/install.bash
+++ test/install.bash
@@ -5,9 +5,12 @@ echo -e "Just some shit2"
 tue-install-info "bla blaa blaat"
 echo -e "Just some shit3"
 
-tue-install-gem-now gitsu
+tue-install-info "bla blaa blaat"
 
-tue-install-pipe sudo apt-get update
+tue-install-warning "bla blaa blaat"
+tue-install-warning "bla blaa blaat"
+
+tue-install-gem-now gitsu
 
 tue-install-cp blaat.bash /opt/ros/noetic
 
