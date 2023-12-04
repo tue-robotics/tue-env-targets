d6646b625012361fc4cc7fda2e3c9c47ee37ca85
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-12-04 21:20:52 +0100

    [split-tue_robocup] target is protected

--- split-tue_robocup/install.bash
+++ split-tue_robocup/install.bash
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-target=~/src/git-split-branch/git-split-branch
+target_source=~/src/git-split-branch/git-split-branch
 link="${TUE_BIN}"/git-split-branch
 
-tue-install-ln "${target}" "${link}"
+tue-install-ln "${target_source}" "${link}"
