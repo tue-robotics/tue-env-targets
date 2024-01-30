03a713e78edb03c0dbe987ded265f9eb44ad4ab2
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2024-01-30 08:34:23 +0100

    [chrome-setup] use separate chrome gpg

--- chrome-setup/install.bash
+++ chrome-setup/install.bash
@@ -3,7 +3,7 @@
 if [[ ! -f /etc/apt/sources.list.d/google-chrome.list ]]
 then
     tue-install-echo "Adding Chrome sources to apt-get"
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/chrome.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     tue-install-apt-get-update
     tue-install-debug "Added Chrome sources to apt-get succesfully"
