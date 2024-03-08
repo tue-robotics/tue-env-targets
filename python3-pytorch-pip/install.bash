4f391d84c8039a705f2b5d36cc259c279e7e964d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2024-03-08 14:41:55 +0100

    [python3-pytorch-pip][TEMP] limit version

--- python3-pytorch-pip/install.bash
+++ python3-pytorch-pip/install.bash
@@ -5,6 +5,6 @@ then
     tue-install-pip torch
     tue-install-pip torchvision
 else
-    tue-install-pip "torch -i https://download.pytorch.org/whl/cpu"
-    tue-install-pip "torchvision -i https://download.pytorch.org/whl/cpu"
+    tue-install-pip "torch==1.13 -i https://download.pytorch.org/whl/cpu"
+    tue-install-pip "torchvision=0.14 -i https://download.pytorch.org/whl/cpu"
 fi
