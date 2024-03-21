f6b6a2119f78a8f88cf618a8af62b8769c1fccc4
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2024-03-21 10:36:37 +0100

    [matthijs]  rebase.updateRefs=true

--- matthijs/install.bash
+++ matthijs/install.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Git Settings
-git_config_items="advice.skippedCherryPicks=false color.ui=always merge.tool=meld pull.ff=true rebase.autoStash=true rerere.enabled=true url.ssh://git@github.com/.insteadOf=https://github.com/"
+git_config_items="advice.skippedCherryPicks=false color.ui=always merge.tool=meld pull.ff=true rebase.autoStash=true rebase.updateRefs=true rerere.enabled=true url.ssh://git@github.com/.insteadOf=https://github.com/"
 for item in $git_config_items
 do
         option=${item%=*}
