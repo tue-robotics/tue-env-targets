3f83276f38514ddae5b0a11b6aa6f1b01b9585e5
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2024-04-26 15:14:50 +0200

    [ros1] update env vars

--- ros1/install.bash
+++ ros1/install.bash
@@ -1,14 +1,19 @@
 #! /usr/bin/env bash
 # shellcheck disable=SC1090
 
-if [ -z "$TUE_ROS_DISTRO" ]
+if [[ ! -v TUE_ENV_ROS_DISTRO && -v TUE_ROS_DISTRO ]]
 then
-    tue-install-error "TUE_ROS_DISTRO was not set"
+    TUE_ENV_ROS_DISTRO=${TUE_ROS_DISTRO}
+    >&2 echo "Change the config of your environment to use 'TUE_ENV_ROS_DISTRO' instead of 'TUE_ROS_DISTRO'"
+fi
+if [[ -z "${TUE_ENV_ROS_DISTRO}" ]]
+then
+    tue-install-error "TUE_ENV_ROS_DISTRO was not set"
     return 1
 fi
 
 # Install basic ROS packages.
-tue-install-system-now ros-"$TUE_ROS_DISTRO"-ros
+tue-install-system-now ros-"${TUE_ENV_ROS_DISTRO}"-ros
 
 if [ ! -d /etc/ros/rosdep ]
 then
@@ -19,16 +24,16 @@ rosdep_update_file="/tmp/tue_rosdep_update_${USER}"
 if [ ! -f "$rosdep_update_file" ]
 then
     tue-install-debug "Updating rosdep"
-    tue-install-pipe rosdep update --rosdistro "$TUE_ROS_DISTRO"
+    tue-install-pipe rosdep update --rosdistro "${TUE_ENV_ROS_DISTRO}"
     touch "$rosdep_update_file"
 fi
 
-mkdir -p "$TUE_SYSTEM_DIR"
+mkdir -p "${TUE_ENV_WS_DIR}"
 
-if [ ! -f "$TUE_SYSTEM_DIR"/devel/setup.bash ]
+if [ ! -f "${TUE_ENV_WS_DIR}"/devel/setup.bash ]
 then
-    [[ -z "${TUE_ROS_VERSION}" ]] && { tue-install-warning "tue-env variable TUE_ROS_VERSION is not set. This will not be allowed in the future.\nSetting TUE_ROS_VERSION=1 temporarily."; }
+    [[ -z "${TUE_ENV_ROS_VERSION}" ]] && { tue-install-warning "tue-env variable TUE_ENV_ROS_VERSION is not set. This will not be allowed in the future.\nSetting TUE_ENV_ROS_VERSION=1 temporarily."; }
     [[ "$CI" == "true" ]] && status_args=" --no-status"
     # shellcheck disable=SC2086
-    TUE_ROS_VERSION=1 tue-make${status_args} || tue-install-error "Error in building the system workspace"
+    TUE_ENV_ROS_VERSION=1 tue-make${status_args} || tue-install-error "Error in building the system workspace"
 fi
