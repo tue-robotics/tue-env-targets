0aab80fc09201c312ac8dc4520702d8048da5d84
frkndmn <ddumanfurkan@gmail.com>
2024-07-04 18:16:36 +0100

    robowflex is added

---
+++ ros-test_franka_gmp_simulation_bringup/install.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+
+if [ ! -d ~/dev/ContactGraspNet/ ]
+then
+	cucr-install-debug "Installing ContactGraspNet"
+	git clone git@github.com:CardiffUniversityComputationalRobotics/ContactGraspNet.git ~/dev/ContactGraspNet
+fi
+
+if [ ! -d ~/dev/UoisSegmentation/ ]
+then
+	cucr-install-debug "Installing UoisSegmentation"
+	git clone git@github.com:CardiffUniversityComputationalRobotics/UoisSegmentation.git ~/dev/UoisSegmentation
+fi
