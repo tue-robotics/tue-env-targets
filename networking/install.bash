326d9b99eb9cbaf8f8220e64ef4f19fd788e6b82
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-08-15 19:46:27 +0200

    [networking] add prompt for permission before updating ssh key (#136)
    * [networking] add prompt for continuing after updating ssh key
    
    * [networking] update ssh key generation

--- networking/install.bash
+++ networking/install.bash
@@ -22,43 +22,65 @@ then
 fi
 
 ## SSH
-ssh_config=~/.ssh/config
-ssh_controlmasters_dir=~/.ssh/controlmasters
-ssh_key=~/.ssh/id_rsa
+SSH_CONFIG=~/.ssh/config
+SSH_CONTROLMASTERS_DIR=~/.ssh/controlmasters
+SSH_KEY_FILE=~/.ssh/id_ed25519
+
+# Allowed key types and their required lengths
+declare -A ALLOWED_KEY_LENGTHS
+ALLOWED_KEY_LENGTHS[rsa]=4096
+ALLOWED_KEY_LENGTHS[ed25519]=256
 
 # Generate ssh key
-generate_ssh="false"
+GENERATE_SSH="true"
 # Generate ssh key when file does not exist yet
-if [ ! -f "$ssh_key" ]
+if ssh-add -l &>/dev/null
 then
-    tue-install-debug "No ssh key exists yet"
-    generate_ssh="true"
+    while IFS= read -r line
+    do
+        key_length=$(echo "${line}" | awk '{print $1}')
+        key_type=$(echo "${line}" | awk '{print $NF}' | tr '[:upper:]' '[:lower:]' | tr -d '()')
+        required_length="${ALLOWED_KEY_LENGTHS[${key_type}]}"
+
+        if [[ -n "${required_length}" && "${key_length}" -ge "${required_length}" ]]
+        then
+            tue-install-debug "Valid ${key_type} key found in agent: ${key_length} bits"
+            GENERATE_SSH="false"
+            break
+        fi
+    done < <(ssh-add -l)
 else
-    # Generate new ssh key if length < 4096
-    if [ "$(ssh-keygen -l -f "$ssh_key" | awk '{print $1}')" -lt 4096 ]
-    then
-        tue-install-info "Generating new ssh key as length < 4096, you might need to copy the new key to the robots, GitHub, etc."
-        generate_ssh="true"
-    else
-        tue-install-debug "ssh key available with length >= 4096"
-    fi
+    tue-install-debug "No SSH keys currently loaded in the agent."
 fi
 
-if [ $generate_ssh == "true" ]
+if [[ ${GENERATE_SSH} == "true" ]]
 then
-    tue-install-debug "Generating ssh key"
-    yes | ssh-keygen -t rsa -b 4096 -N "" -f "$ssh_key"
-    rm -r ${ssh_controlmasters_dir:?}/* 2>/dev/null # close all connections, to prevent any possible weird behaviour
-    ssh-add # Start using the new key
+    echo
+    while true
+    do
+        exec < /dev/tty
+        read -p $'\e[1m[networking]\e[0m: No valid ssh key found. Would you like to generate a new ED25519 key? [y/n] ' -n 1 -r
+        exec <&-
+        echo
+        if [[ ${REPLY} =~ ^[Yy]$ ]]
+        then
+            tue-install-debug "Generating new SSH key (ED25519)"
+            [[ -f ${SSH_KEY_FILE} ]] && tue-install-pipe ssh-add -d ${SSH_KEY_FILE}
+            yes | ssh-keygen -t ed25519 -N "" -f "${SSH_KEY_FILE}"
+            rm -r ${SSH_CONTROLMASTERS_DIR:?}/* 2>/dev/null
+            tue-install-pipe ssh-add ${SSH_KEY_FILE}
+            tue-install-info "Succesfully generated a new SSH key (${SSH_KEY_FILE}), don't forget to register the new public key at the required services"
+            break
+        elif [[ ${REPLY} =~ ^[Nn]$ ]]
+        then
+            tue-install-info "Skipping SSH key generation"
+            break
+        fi
+        echo -e "\e[1mPlease answer with 'y' or 'n'\e[0m"
+    done
 fi
 
 # Enable persistent connection multiplexing
-if [ ! -f $ssh_config ]
-then
-    touch $ssh_config
-fi
-if [ ! -d $ssh_controlmasters_dir ]
-then
-    mkdir -p $ssh_controlmasters_dir
-fi
-tue-install-add-text ssh_persistent_connection_lines $ssh_config
+[[ -f ${SSH_CONFIG} ]] || touch ${SSH_CONFIG}
+[[ -d ${SSH_CONTROLMASTERS_DIR} ]] || mkdir -p ${SSH_CONTROLMASTERS_DIR}
+tue-install-add-text ssh_persistent_connection_lines ${SSH_CONFIG}
