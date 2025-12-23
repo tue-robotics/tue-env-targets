957491bd11b9cbde426f582837009e0bc71c4db2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-12-23 21:07:13 +0100

    [cuda] implement tue-install-apt-wait-for-lock

--- cuda/install.bash
+++ cuda/install.bash
@@ -19,6 +19,7 @@ then
     then
         return
     else
+        tue-install-apt-wait-for-lock
         sudo apt-get purge -y cuda*
         sudo apt-get purge libcudnn*
         sudo apt-get purge libcudnn*-dev
@@ -33,7 +34,7 @@ then
     tue-install-debug "Installing cuda with following configuration:\nUbuntu version: ${ubuntu_version}\nCuda version: ${cuda_version}\nArchitecture: ${architecture}"
 
     cuda_file="cuda-repo-ubuntu${ubuntu_version}_${cuda_version}_${architecture}.deb"
-    cuda_url="""https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/${cuda_file}"
+    cuda_url="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/${cuda_file}"
 
     if [ -f "/tmp/$cuda_file" ]
     then
@@ -41,17 +42,14 @@ then
         rm -f "/tmp/$cuda_file"
     fi
 
-    tue-install-debug "wget $cuda_url"
-    wget "$cuda_url" -P /tmp
+    tue-install-pipe wget "${cuda_url}" -P /tmp
 
-    tue-install-debug "sudo dpkg -i /tmp/$cuda_file"
-    sudo dpkg -i "/tmp/$cuda_file"
+    tue-install-apt-wait-for-lock
+    tue-install-pipe sudo dpkg -i "/tmp/${cuda_file}"
 
     tue-install-debug "sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub"
     sudo apt-key adv --fetch-keys "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/$(uname -p)/7fa2af80.pub"
 
-    tue-install-debug "sudo apt-get update -qq"
-    sudo apt-get update -qq
-
+    tue-install-apt-get-update
     tue-install-system cuda
 fi
