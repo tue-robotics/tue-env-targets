8194cb63417b31a587c9a76ccde0049df23f9b7c
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2026-01-27 20:51:30 +0100

    [hero-chrony-client-pc] fix (re)starting and enabling of service

--- hero-chrony-client-pc/install.bash
+++ hero-chrony-client-pc/install.bash
@@ -1,16 +1,40 @@
 #! /usr/bin/env bash
 set -e
 
+service_name="chrony.service"
+
+needs_restart=false
 # Check config file
 if ! cmp /etc/chrony/chrony.conf "$(dirname "${BASH_SOURCE[0]}")"/chrony.conf --quiet
 then
-    tue-install-echo "Chrony config is probably not correct, will copy"
+    tue-install-echo "Chrony config is not correct, will copy"
+    needs_restart=true
 
     # Copy and backup old config
     tue-install-pipe sudo install --backup=numbered --compare --verbose "$(dirname "${BASH_SOURCE[0]}")"/chrony.conf /etc/chrony/chrony.conf
-
-    # Restart chrony
-    tue-install-pipe sudo service chrony restart
+else
+    tue-install-debug "Chrony config is the same, not copying"
+    if ! systemctl is-active -q ${service_name}
+    then
+        tue-install-debug "Chrony is not active yet"
+        needs_restart=true
+    fi
 fi
+
+if [[ ${needs_restart} == true ]]
+then
+    tue-install-echo "(Re)starting chrony"
+    tue-install-pipe sudo systemctl restart ${service_name}
+else
+    tue-install-debug "No need to restart chrony"
+fi
+
+if ! systemctl is-enabled -q ${service_name}
+then
+    tue-install-echo "Chrony is not enabled yet"
+    tue-install-pipe sudo systemctl enable ${service_name}
+fi
+
+unset needs_restart service_name
 
 set +e
