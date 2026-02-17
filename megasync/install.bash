90668d47f80b07c0e45631d11f7ebc493979a1bc
Hossein Zahedi <h.zahedi1920@gmail.com>
2026-02-17 19:45:40 +0100

    [megasync] fix debian link (#513)
    Co-authored-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- megasync/install.bash
+++ megasync/install.bash
@@ -1,21 +1,21 @@
 #! /usr/bin/env bash
 
-DATA_DIR="$HOME/MEGA"
-if [ ! -d "$DATA_DIR" ]
+DATA_DIR="${HOME}/MEGA"
+if [[ ! -d "${DATA_DIR}" ]]
 then
     # Download mega sync
     UBUNTU_VERSION=$(lsb_release -sr)
-    tue-install-debug "Ubuntu version: $UBUNTU_VERSION"
+    tue-install-debug "Ubuntu version: ${UBUNTU_VERSION}"
 
-    LINK="https://mega.nz/linux/MEGAsync/xUbuntu_""$UBUNTU_VERSION""/amd64/megasync-xUbuntu_""$UBUNTU_VERSION""_amd64.deb"
-    DEST="/tmp/""$UBUNTU_VERSION""_MEGA_SYNC.deb"
+    LINK="https://mega.nz/linux/repo/xUbuntu_${UBUNTU_VERSION}/amd64/megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb"
+    DEST="/tmp/${UBUNTU_VERSION}_MEGA_SYNC.deb"
 
-    tue-install-debug "Downloading $LINK to $DEST"
-    wget "$LINK" -O "$DEST"
+    tue-install-debug "Downloading ${LINK} to ${DEST}"
+    wget "${LINK}" -O "${DEST}"
 
     # Install
     tue-install-debug "Installing debian package ..."
-    tue-install-dpkg "$DEST"
+    tue-install-dpkg "${DEST}"
 
     tue-install-info "NOW Please configure the mega client with the AMIGO credentials"
 fi
